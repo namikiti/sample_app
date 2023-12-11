2edb370dcf856580510ca726c6f1328868fc230e
EC2 Default User <ec2-user@ip-172-31-22-55.ap-northeast-1.compute.internal>
2023-12-11 13:30:13 +0000

    4章完了

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,7 +8,7 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
   def index
@@ -16,11 +16,12 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
   end
-  
+
 
   private
 
