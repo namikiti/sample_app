8915c3876b004dfc447d512a7b4612f4f30420ba
EC2 Default User <ec2-user@ip-172-31-22-55.ap-northeast-1.compute.internal>
2023-12-17 09:27:56 +0000

    10章完了

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -31,7 +31,7 @@ class ListsController < ApplicationController
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])
     list.destroy
